7324b7c756e38a597f65fb2096b40e52c366f30d
Antonio Vallez <arvallez113@students.ecsu.edu>
2022-07-20 05:50:27 -0400

    Filter out Emojis

--- src/scenes/interface/game/main/Main.js
+++ src/scenes/interface/game/main/Main.js
@@ -755,6 +755,7 @@ export default class Main extends BaseScene {
 
     onChatSend() {
         let text = this.chatInput.text
+        text = text.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '');
 
         this.chatInput.clearText()
 
