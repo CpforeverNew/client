27f721485afe88314ac75d8911117eaf8296fc41
Keir Davis <minecraftfun201@gmail.com>
2022-08-07 22:18:12 +0100

    Draggable players with holding shift

--- src/engine/network/plugins/plugins/Actions.js
+++ src/engine/network/plugins/plugins/Actions.js
@@ -6,6 +6,7 @@ export default class Actions extends Plugin {
     constructor(network) {
         super(network)
         this.events = {
+            'send_drag_position': this.send_drag_position,
             'send_position': this.sendPosition,
             'send_frame': this.sendFrame,
             'snowball': this.snowball
@@ -14,6 +15,21 @@ export default class Actions extends Plugin {
 
     get room() {
         return this.world.room
+    }
+
+    
+    send_drag_position(args) {
+        if (!this.room.isReady) {
+            return this.room.updateWaiting(args.id, { x: args.x, y: args.y, frame: 1 })
+        }
+
+        if (this.room.penguins[args.id]){
+            this.room.penguins[args.id].setPos(args.x, args.y)
+
+            // update the name tag position
+            this.room.penguins[args.id].nameTag.x = args.x
+            this.room.penguins[args.id].nameTag.y = args.y + 40
+        } 
     }
 
     sendPosition(args) {
--- src/engine/world/penguin/loader/PenguinLoader.js
+++ src/engine/world/penguin/loader/PenguinLoader.js
@@ -1,114 +1,174 @@
-import SpriteLoader from './SpriteLoader'
-import PuffleLoader from './PuffleLoader'
+import SpriteLoader from "./SpriteLoader";
+import PuffleLoader from "./PuffleLoader";
 
 export default class PenguinLoader extends SpriteLoader {
+  constructor(world) {
+    super(world);
 
-    constructor(world) {
-        super(world)
+    this.nameStyle = {
+      fontFamily: "Burbank Small",
+      fontSize: 24,
+      color: "#000000",
+      align: "center",
+      fixedWidth: 250,
+    };
+  }
 
-        this.nameStyle = {
-            fontFamily: 'Burbank Small',
-            fontSize: 24,
-            color: '#000000',
-            align: 'center',
-            fixedWidth: 250
-        }
+  loadPenguin(penguin) {
+    this.ploader = new PuffleLoader(penguin);
 
+    this.addPenguin(penguin);
+    this.addShadow(penguin);
+    this.addInput(penguin);
+  }
+
+  addPenguin(penguin) {
+    this.loadSprite(penguin, "penguin_body", 1);
+    this.loadSprite(penguin, "penguin", 2);
+  }
+
+  addShadow(penguin) {
+    let shadow = penguin.room.add.image(0, 0, "penguin_base", "shadow");
+
+    penguin.addAt(shadow, 0);
+  }
+
+  addRing(penguin) {
+    let ring = penguin.room.add.image(0, 0, "penguin_base", "ring");
+
+    penguin.addAt(ring, 0);
+  }
+
+  addName(penguin) {
+    let x = penguin.x;
+    let y = penguin.y + 40;
+    if (penguin.username_approved == 1) {
+      var nameTag = penguin.room.add.text(
+        x,
+        y,
+        penguin.username,
+        this.nameStyle
+      );
+    }
+    if (penguin.username_approved != 1) {
+      var nameTag = penguin.room.add.text(
+        x,
+        y,
+        "P" + penguin.id,
+        this.nameStyle
+      );
     }
 
-    loadPenguin(penguin) {
-        this.ploader = new PuffleLoader(penguin)
+    nameTag.setOrigin(0.5);
+    nameTag.depth = penguin.depth + 2000; // Keep nametag above everything else
 
-        this.addPenguin(penguin)
-        this.addShadow(penguin)
-        this.addInput(penguin)
+    return nameTag;
+  }
+
+  addInput(penguin) {
+    let hitArea = new Phaser.Geom.Ellipse(0, -20, 70, 90);
+
+    penguin.setInteractive({
+      cursor: "pointer",
+      hitArea: hitArea,
+      hitAreaCallback: Phaser.Geom.Ellipse.Contains,
+    });
+
+    penguin.on("pointerup", () => this.onPenguinClick(penguin.id));
+
+    penguin.on("pointerdown", () =>
+      this.onPenguinDrag(penguin.id, this.world.input.activePointer)
+    );
+    penguin.on("pointerup", () => this.onPenguinDragEnd(penguin.id));
+  }
+
+  onPenguinClick(id) {
+    // check if shift is pressed
+    var keyObj = this.world.room.input.keyboard.addKey("SHIFT"); // Get key object
+    var isDown = this.world.room.input.keyboard.checkDown(keyObj);
+
+    if (!isDown) {
+      this.world.interface.showCard(id);
     }
+  }
 
-    addPenguin(penguin) {
-        this.loadSprite(penguin, 'penguin_body', 1)
-        this.loadSprite(penguin, 'penguin', 2)
+  onPenguinDrag(id, pointer) {
+    if (this.world.client.penguin.rank < 4) {
+      return;
     }
+    // check if shift is pressed
+    var keyObj = this.world.room.input.keyboard.addKey("SHIFT"); // Get key object
+    var isDown = this.world.room.input.keyboard.checkDown(keyObj);
 
-    addShadow(penguin) {
-        let shadow = penguin.room.add.image(0, 0, 'penguin_base', 'shadow')
-
-        penguin.addAt(shadow, 0)
+    if (isDown) {
+      this.world.input.on("pointermove", (pointer) =>
+        this.onPenguinDragMove(id, pointer)
+      );
     }
+  }
 
-    addRing(penguin) {
-        let ring = penguin.room.add.image(0, 0, 'penguin_base', 'ring')
+  onPenguinDragMove(id, pointer) {
+    this.world.room.penguins[id].x = pointer.x;
+    this.world.room.penguins[id].y = pointer.y;
 
-        penguin.addAt(ring, 0)
+    // update the name tag position
+    this.world.room.penguins[id].nameTag.x = pointer.x;
+    this.world.room.penguins[id].nameTag.y = pointer.y + 40;
+  }
+
+  onPenguinDragEnd(id) {
+    this.world.input.off("pointermove");
+    this.world.input.off("pointerup");
+    this.world.network.send("send_drag_position", {
+      playerID: id,
+      x: this.world.room.penguins[id].x,
+      y: this.world.room.penguins[id].y,
+    });
+  }
+
+  addPuffle(penguin, puffle) {
+    let color = this.world.crumbs.puffles[puffle].name;
+    this.ploader.loadPuffle(penguin, color);
+  }
+
+  loadPuffle(penguin, color) {
+    if (!penguin.puffle || !penguin.active || penguin.hasPuffle) return;
+
+    penguin.pufflesprite = penguin.room.add.sprite(
+      60,
+      0,
+      `puffle_${color}`,
+      "1_1"
+    );
+    penguin.pufflesprite.depth = 3;
+
+    penguin.add(penguin.pufflesprite);
+    penguin.pufflesprite.color = color;
+    penguin.hasPuffle = true;
+
+    if (this.world.client.penguin.id == this.world.room.id) {
+      this.addPuffleInput(penguin);
     }
+  }
 
-    addName(penguin) {
-        let x = penguin.x
-        let y = penguin.y + 40
-        if (penguin.username_approved == 1) {
-            var nameTag = penguin.room.add.text(x, y, penguin.username, this.nameStyle)
-        }
-        if (penguin.username_approved != 1) {
-            var nameTag = penguin.room.add.text(x, y, "P" + penguin.id, this.nameStyle)
-        }
+  addPuffleInput(penguin) {
+    // creates the hitbox to open the puffle care menu
+    penguin.pufflesprite.hitArea = new Phaser.Geom.Ellipse(25, 25, 50, 50);
+    penguin.pufflesprite.setInteractive({
+      cursor: "pointer",
+      hitArea: penguin.pufflesprite.hitArea,
+      hitAreaCallback: Phaser.Geom.Ellipse.Contains,
+    });
+    penguin.pufflesprite.on("pointerup", () =>
+      this.onPuffleClick(penguin.puffle)
+    );
+    penguin.pufflesprite.isButton = true;
+  }
 
-        nameTag.setOrigin(0.5)
-        nameTag.depth = penguin.depth + 2000 // Keep nametag above everything else
-
-        return nameTag
-    }
-
-    addInput(penguin) {
-        let hitArea = new Phaser.Geom.Ellipse(0, -20, 70, 90)
-
-        penguin.setInteractive({
-            cursor: 'pointer',
-            hitArea: hitArea,
-            hitAreaCallback: Phaser.Geom.Ellipse.Contains
-        })
-
-        penguin.on('pointerup', () => this.onPenguinClick(penguin.id))
-    }
-
-    onPenguinClick(id) {
-        this.world.interface.showCard(id)
-    }
-
-    addPuffle(penguin, puffle) {
-        let color = this.world.crumbs.puffles[puffle].name
-        this.ploader.loadPuffle(penguin,color)
-    }
-
-    loadPuffle(penguin, color) {
-        if (!penguin.puffle || !penguin.active || penguin.hasPuffle) return
-
-        penguin.pufflesprite = penguin.room.add.sprite(60, 0, `puffle_${color}`, '1_1')
-        penguin.pufflesprite.depth = 3
-
-        penguin.add(penguin.pufflesprite)
-        penguin.pufflesprite.color = color
-        penguin.hasPuffle = true
-
-        if (this.world.client.penguin.id == this.world.room.id) {
-            this.addPuffleInput(penguin)
-        }
-    }
-
-    addPuffleInput(penguin) {
-        // creates the hitbox to open the puffle care menu
-        penguin.pufflesprite.hitArea = new Phaser.Geom.Ellipse(25, 25, 50, 50)
-        penguin.pufflesprite.setInteractive({
-            cursor: 'pointer',
-            hitArea: penguin.pufflesprite.hitArea,
-            hitAreaCallback: Phaser.Geom.Ellipse.Contains
-        })
-        penguin.pufflesprite.on('pointerup', () => this.onPuffleClick(penguin.puffle))
-        penguin.pufflesprite.isButton = true
-    }
-
-    onPuffleClick(puffle) {
-        // sends a packet to the server which requests the wellbeing information for the care menu to use
-        this.world.network.send('get_wellbeing', {
-            puffle: puffle
-        })
-    }
+  onPuffleClick(puffle) {
+    // sends a packet to the server which requests the wellbeing information for the care menu to use
+    this.world.network.send("get_wellbeing", {
+      puffle: puffle,
+    });
+  }
 }
