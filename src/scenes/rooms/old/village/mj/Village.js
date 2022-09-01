ee2f5b0798a1823f2082adfb93bee2f7b8257e18
Keir Davis <minecraftfun201@gmail.com>
2022-09-01 03:43:48 +0100

    Cove and Village

--- src/scenes/rooms/old/village/mj/Village.js
+++ src/scenes/rooms/old/village/mj/Village.js
@@ -21,7 +21,7 @@ export default class Village extends RoomScene {
             'dock': () => this.triggerRoom(800, 436, 280),
             'beach': () => this.triggerRoom(400, 920, 360),
             'mtn': () => this.triggerRoom(230, 840, 320),
-            'phone': null
+            'winter': null
         }
 
         /* END-USER-CTR-CODE */
@@ -37,87 +37,70 @@ export default class Village extends RoomScene {
     _create() {
 
         // bg
-        const bg = this.add.image(-25, -15, "village", "bg");
-        bg.setOrigin(0, 0);
+        this.add.image(771, 409, "skimj", "bg");
 
-        // lodge_door
-        const lodge_door = this.add.image(1075, 318, "village", "lodge_door");
-        lodge_door.setOrigin(0.27807486631016043, 0.5560538116591929);
+        // door10001
+        this.add.image(1173, 320, "skimj", "Door10001");
 
-        // lodge_stairs
-        this.add.image(972, 444, "village", "lodge_stairs");
+        // lodge1
+        this.add.image(1206, 281, "skimj", "Lodge1");
 
-        // lodge_front
-        const lodge_front = this.add.image(1087, 389, "village", "lodge_front");
-        lodge_front.setOrigin(0.4807692307692308, 0.5352112676056338);
+        // sportsShop
+        this.add.image(782, 366, "skimj", "SportsShop");
 
-        // lodge_snow
-        const lodge_snow = this.add.image(1104, 429, "village", "lodge_snow");
-        lodge_snow.setOrigin(0.5368217054263565, 0.4838709677419355);
+        // sign3
+        this.add.image(113, 673, "skimj", "Sign3");
 
-        // phone
-        this.add.image(1373, 316, "village", "phone");
+        // sign4
+        const sign4 = this.add.image(385, 666, "skimj", "Sign4");
 
-        // phone_door
-        const phone_door = this.add.image(1350, 387, "village", "phone_door");
-        phone_door.setOrigin(0.22797927461139897, 0.6206896551724138);
-
-        // phone_snow
-        const phone_snow = this.add.image(1361, 595, "village", "phone_snow");
-        phone_snow.setOrigin(0.509090909090909, 0.5272727272727272);
+        // tourStand0001
+        this.add.image(125, 319, "skimj", "TourStand0001");
 
         // lift
-        this.add.image(348, 200, "village", "lift");
-
-        // smoke
-        const smoke = this.add.sprite(1110, 32, "village", "smoke0001");
-
-        // left_sign
-        const left_sign = this.add.image(133, 677, "village", "left_sign");
-        left_sign.setOrigin(0.4444444444444444, 0.5217391304347826);
-
-        // right_sign
-        const right_sign = this.add.image(1471, 668, "village", "right_sign");
-        right_sign.setOrigin(0.4861111111111111, 0.7831325301204819);
-
-        // tours
-        const tours = this.add.image(792, 483, "village", "tours");
-        tours.setOrigin(0.5, 0.8846153846153846);
-
-        // tours_text
-        const tours_text = this.add.image(794, 505.2201404552512, "village", "tours_text");
-        tours_text.setOrigin(0.5, 3.9212529261510674);
+        this.add.image(274, 178, "village", "lift");
 
         // chair
-        const chair = this.add.sprite(292, 150, "village", "chair0001");
+        const chair = this.add.sprite(218, 128, "village", "chair0001");
+
+        // sign2
+        this.add.image(1475, 632, "skimj", "Sign2");
+
+        // sledge1
+        this.add.image(1440, 443, "skimj", "Sledge1");
+
+        // tuba10001
+        this.add.image(922, 356, "skimj", "Tuba10001");
+
+        // stand50001
+        const stand50001 = this.add.image(872, 443, "skimj", "Stand50001");
+
+        // stand80001
+        const stand80001 = this.add.image(757, 449, "skimj", "Stand80001");
+
+        // stand10001
+        const stand10001 = this.add.image(847, 475, "skimj", "Stand10001");
+
+        // stand20001
+        const stand20001 = this.add.image(774, 476, "skimj", "Stand20001");
+
+        // stand30001
+        const stand30001 = this.add.image(694, 478, "skimj", "Stand30001");
+
+        // stand40001
+        const stand40001 = this.add.image(955, 420, "skimj", "Stand40001");
+
+        // stand60001
+        const stand60001 = this.add.image(577, 423, "skimj", "Stand60001");
+
+        // stand70001
+        const stand70001 = this.add.image(660, 443, "skimj", "Stand70001");
+
+        // sign1
+        const sign1 = this.add.image(964, 483, "skimj", "Sign1");
 
         // lists
-        const sort = [tours, tours_text];
-
-        // lodge_door (components)
-        const lodge_doorButton = new Button(lodge_door);
-        lodge_doorButton.spriteName = "lodge_door";
-        lodge_doorButton.activeFrame = false;
-        const lodge_doorMoveTo = new MoveTo(lodge_door);
-        lodge_doorMoveTo.x = 1000;
-        lodge_doorMoveTo.y = 400;
-
-        // phone_door (components)
-        const phone_doorButton = new Button(phone_door);
-        phone_doorButton.spriteName = "phone_door";
-        phone_doorButton.activeFrame = false;
-        const phone_doorMoveTo = new MoveTo(phone_door);
-        phone_doorMoveTo.x = 1340;
-        phone_doorMoveTo.y = 480;
-
-        // smoke (components)
-        const smokeAnimation = new Animation(smoke);
-        smokeAnimation.key = "smoke";
-        smokeAnimation.end = 5;
-
-        // tours (components)
-        const toursSimpleButton = new SimpleButton(tours);
-        toursSimpleButton.callback = () => this.interface.prompt.showItem(428);
+        const sort = [stand40001, sign1, stand50001, stand10001, stand20001, stand80001, stand30001, stand70001, stand60001, sign4];
 
         // chair (components)
         const chairAnimation = new Animation(chair);
